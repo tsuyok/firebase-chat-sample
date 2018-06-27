c607826f2787bc4b31c0817e9bfd3efb70166a41
Tsuyoshi Yasunishi <tsuyok@gmail.com>
2018-06-27 23:01:41 +0900

    chat作成

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,126 @@
 <template>
   <div id="app">
-    <img src="assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="panel-default">
+      <div class="panel-heading">
+        <p>リアルタイムチャットサンプル</p>
+      </div>
+      <div id="scroller" class="panel-body">
+        <ul>
+          <il v-for="item in list">
+            <span v-if="item.name == username">
+              <p class='sender_name me'>{{item.name}}</p><p class='right_balloon'>{{item.message}}</p><p class='clear_balloon'></p>
+            </span>
+            <span v-if="item.name != username">
+              <p class='sender_name'>{{item.name}}</p><p class='right_balloon'>{{item.message}}</p><p class='clear_balloon'></p>
+            </span>
+          </il>
+        </ul>
+      </div>
+      <div class="panel-footer">
+        <input type='text' class="form-control" v-model="username" id="nameInput" placeholder="ユーザー名を入力してください">
+      </div>
+      <div class="panel-footer">
+        <input type='text' class="form-control" v-model="message" id="messageInput" placeholder="メッセージ内容を入力してください">
+      </div>
+      <div>
+        <button type="button" class="btn btn-default" @click="sendMessage">
+          送信
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+  import firebase from 'firebase'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      list: [],
+      username: '',
+      message: ''
+    }
+  },
+  created () {
+    this.listen();
+  },
+  methods: {
+    listen () {
+      firebase.database().ref('myBoard/').on('value', snapshot => { // eslint-disable-line
+        if (snapshot) {
+          const rootList = snapshot.val();
+          let list = [];
+          Object.keys(rootList).forEach((val, key) => {
+            rootList[val].id = val;
+            list.push(rootList[val]);
+          })
+          this.list = list;
+        }
+      })
+    },
+    sendMessage () {
+      if (!this.username || !this.message) return
+      firebase.database().ref('myBoard/').push({
+        name: this.username,
+        message: this.message
+      });
+      this.username = ''
+      this.message = ''
     }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  ul,li{
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+    text-align: left;
+  }
+  #scroller {
+    height: 500px;
+    overflow: auto;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .left_balloon {
+    position: relative;
+    background: #f1f0f0;
+    border: 0px solid #777;
+    margin: 5px 10px;
+    padding: 5px 10px;
+    border-radius: 15px;
+    width: 400px;
+    clear: both;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .right_balloon {
+    color: #fff;
+    position: relative;
+    background: #0084ff;
+    border: 0px solid #777;
+    margin: 5px 10px;
+    padding: 5px 10px;
+    border-radius: 15px;
+    width: 400px;
+    clear: both;
+    float: right;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  .clear_balloon{
+    clear: both;
+  }
 
-a {
-  color: #42b983;
-}
+  .sender_name{
+    margin-bottom: -5px;
+    padding-left: 20px;
+    color: rgba(0, 0, 0, .40);
+  }
+  .sender_name.me{
+    float: right;
+    padding: 0 20px 0 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
+import config from './firebaseConfig'
+
+Vue.config.productionTip = false
+
+firebase.initializeApp(config);
 
 new Vue({
   el: '#app',
